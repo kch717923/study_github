751ee7a4887396fea67d52c0b178cdfd17733109
김창휘 <kch717923@gmail.com>
2021-11-23 18:06:56 +0900

    복잡하다

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -5,6 +5,23 @@ import TodoList from './componnents/TodoList';
 import './css/bundle.css';
 
 function App() {
+	const [todos, setTodos] = useState([
+		{
+			id:1,
+			text:'첫번째 메모',
+			checked:true
+		},
+		{
+			id:2,
+			text:'두번째 메모',
+			checked:true
+		},
+		{
+			id:3,
+			text:'세번째 메모',
+			checked:false
+		},
+	])
 	return (
 		<TodoTemplate>
 			<TodoInsert/>
--- react_test/src/componnents/TodoList.js
+++ react_test/src/componnents/TodoList.js
@@ -4,7 +4,7 @@ import TodoListItem from './TodoListItem';
 const TodoList = ({ todos }) => {
     return (
         <div className="TodoList">
-            {todos.map((todo) => {return <TodoListItem todo={todo} key={todo.id}/>})}
+            테스트
         </div>
     );
 };
